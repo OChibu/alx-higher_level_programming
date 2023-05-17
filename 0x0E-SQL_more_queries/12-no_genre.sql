4ffe7463d75682431b57e7d2d670d9e78793a6a9
OChibu <beze4luv@yahoo.com>
2023-05-17 05:33:33 -0700

    SQL - More queries

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,5 @@
---script I provided lists all shows contained in the "hbtn_0d_tvshows"
+--script provide lists all shows contained in the "hbtn_0d_tvshows"
 --It displays the show title and the genre ID
--- e and the genre ID (which will be NULL for shows without a genre
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
