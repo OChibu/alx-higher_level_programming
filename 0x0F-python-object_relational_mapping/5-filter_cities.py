85e83b58246a549e65cdbeb832bae8fa12eb0516
OChibu <beze4luv@yahoo.com>
2023-06-19 15:52:22 -0700

    Python Object relational mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""
-  takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-
-
-def main():
-    from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], database=argv[3])
-
-    cur = db.cursor()
-
-    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id\
-        = states.id WHERE states.name LIKE %s ORDER BY cities.id", (argv[4],))
-    print(", ".join(rows[0] for rows in cur.fetchall()))
-
-    cur.close()
-    db.close()
+import sys
 
 
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
