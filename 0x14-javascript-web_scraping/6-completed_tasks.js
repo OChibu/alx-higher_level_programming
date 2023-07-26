021d904d3edbe3f99714494301874df2e8656ec0
OChibu <beze4luv@yahoo.com>
2023-07-26 06:48:11 -0700

    web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,28 +1,63 @@
 #!/usr/bin/node
+
+const URL = process.argv[2];
 const request = require('request');
 
-const url = process.argv[2];
-
-request.get(url, (error, response, body) => {
+request.get(URL, { json: true }, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    const tasks = JSON.parse(body);
-
-    const completedTasksByUser = {};
-
-    tasks.forEach((task) => {
-      if (task.completed) {
-        const userId = task.userId;
-        completedTasksByUser[userId] = completedTasksByUser[userId] ? completedTasksByUser[userId] + 1 : 1;
-      }
-    });
-
-    console.log(completedTasksByUser);
+    console.log(error);
+    return;
   }
-});
 
-    const formattedOutput = JSON.stringify(completedTasksByUser, null, 2);
-    console.log(formattedOutput);
+  if (response.statusCode !== 200) {
+    console.log(error);
+    return;
+  }
+
+  const todos = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!todos[todo.userId]) {
+        todos[todo.userId] = 1;
+      } else {
+        todos[todo.userId]++;
+      }
+    }
+  });
+
+  const length = Object.keys(todos).length;
+  if (length === 0) {
+    console.log('{}');
+    return;
+  }
+
+  if (length === 2) {
+    let output = '{ ';
+    let isFirst = true;
+    for (const key in todos) {
+      if (!isFirst) {
+        output += ', ';
+      }
+      output += `'${key}': ${todos[key]}`;
+      isFirst = false;
+    }
+    output += ' }';
+    console.log(output);
+  } else {
+    let task = 0;
+    for (const key in todos) {
+      if (task === 0) {
+        if (length !== 1) {
+          console.log('{ \'' + key + '\': ' + todos[key] + ',');
+        } else {
+          console.log('{ \'' + key + '\': ' + todos[key] + ' }');
+        }
+      } else if (task === length - 1) {
+        console.log('  \'' + key + '\': ' + todos[key] + ' }');
+      } else {
+        console.log(`  '${key}': ${todos[key]},`);
+      }
+      task++;
+    }
   }
 });
